606c80d1cf82da6b320943f3e555559c462d7614
K1 <Keyvan.Ghanizadeh@Gmail.com>
2024-05-22 20:51:34 +0200

    finished usecase4

--- app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,47 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                val versionFeaturesList = recentAndroidVersions.map {
+                    mockApi.getAndroidVersionFeatures(it.apiLevel)
+                }
+                uiState.value = UiState.Success(versionFeaturesList)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed")
+            }
 
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val deferredList = mockApi.getRecentAndroidVersions().map {
+                    async {
+                        mockApi.getAndroidVersionFeatures(it.apiLevel)
+                    }
+                }
+                uiState.value = UiState.Success(deferredList.awaitAll())
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed")
+            }
+
+        }
 
     }
 }
