ef6e5df6de2d555c80172fa41958e61950c651c5
K1 <Keyvan.Ghanizadeh@Gmail.com>
2024-05-23 14:22:57 +0200

    finished usecase8

--- app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,6 +12,30 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
+        viewModelScope.launch {
+            val androidVersions = database.getAndroidVersions()
+            if (androidVersions.isEmpty()) {
+                uiState.value = UiState.Error(DataSource.DATABASE, "No data found in Database")
+            } else {
+                uiState.value = UiState.Success(
+                    dataSource = DataSource.DATABASE,
+                    androidVersions.mapToUiModelList()
+                )
+            }
+            uiState.value = UiState.Loading.LoadFromNetwork
+            try {
+                val androidVersionsFromNetwork = api.getRecentAndroidVersions()
+                androidVersionsFromNetwork.forEach {
+                    database.insert(it.mapToEntity())
+                }
+                uiState.value = UiState.Success(DataSource.NETWORK, androidVersionsFromNetwork)
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error(DataSource.NETWORK, "Something went wrong")
+            }
+
+        }
 
     }
 
