47dae14dc2e180ae538f18f36038033b12d874da
K1 <Keyvan.Ghanizadeh@Gmail.com>
2024-05-23 12:36:50 +0200

    finished usecase6

--- app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/kotlin/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,57 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 3
+
+            try {
+                retry(numberOfRetries) {
+                    loadRecentAndroidVersions()
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network Request Failed")
+            }
+        }
+    }
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelayMillis = initialDelayMillis
+
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+            delay(currentDelayMillis)
+            currentDelayMillis = (currentDelayMillis * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+        return block()
+    }
+
+    private suspend fun loadRecentAndroidVersions() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
